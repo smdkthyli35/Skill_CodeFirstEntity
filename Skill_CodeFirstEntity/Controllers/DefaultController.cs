84ae55c4a93c0e9e2bfa3867a7c026821b31e3ce
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-03-28 15:28:47 +0300

    Add project files.

---
+++ Skill_CodeFirstEntity/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Skill_CodeFirstEntity.Models.Siniflar;
+
+namespace Skill_CodeFirstEntity.Controllers
+{
+    public class DefaultController : Controller
+    {
+        CONTEXT c = new CONTEXT();
+        // GET: Default
+        public ActionResult Index()
+        {
+            var degerler = c.YETENEKLERS.ToList();
+            return View(degerler);
+        }
+    }
+}
